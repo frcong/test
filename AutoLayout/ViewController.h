71217f9a274e776fd5ac7345f94918652de8d9ff
ruicong <fengruicong@qfpay.com>
2017-03-10 17:31:36 +0800

    初始化项目

---
+++ AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout
+//
+//  Created by 李林 on 16/5/30.
+//  Copyright © 2016年 李林. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# test
-test
+#jenkins_ipa
